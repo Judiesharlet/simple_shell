18879fb67471d523df5dfb719e0f6b7aeaf01076
Joan254 <joanotieno1998@gmail.com>
2023-08-23 11:26:36 +0000

    Updated

--- main.h
+++ main.h
@@ -11,12 +11,17 @@
 
 extern char **environ;
 
+int _strlen(char *s);
+int _strcmp(char *s1, char *s2);
+char *_strdup(char *s);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
 void print_prompt(char *prompt);
 char **tokenizeLine(char *str, const char *delim);
 void execute_command(char **av);
+void execute_pipeline(char **av, int pipe_index);
 char *get_path(char *command);
 int exit_command(char *command);
 void print_env(void);
 
 #endif
-
