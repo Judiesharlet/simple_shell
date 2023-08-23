8d217f18112399a09ce2610884e914e6f656048c
Judiesharlet <judithnjoki6@gmail.com>
2023-08-23 14:09:56 +0300

    print_env.c

--- print_env.c
+++ print_env.c
@@ -8,13 +8,13 @@
 
 void print_env(void)
 {
-        char **env_var = environ;
+	char **env_var = environ;
 
-        while (*env_var != NULL)
-        {
-                write(STDOUT_FILENO, *env_var, strlen(*env_var));
-                write(STDOUT_FILENO, "\n", 1);
-                env_var++;
-        }
+	while (*env_var != NULL)
+	{
+	write(STDOUT_FILENO, *env_var, strlen(*env_var));
+	write(STDOUT_FILENO, "\n", 1);
+	env_var++;
+	}
 }
 
